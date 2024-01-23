39558a02e69348b101ef3f751f5dc6582d67cda7
AntonyBl4ck <133582398+AntonyBl4ck@users.noreply.github.com>
2024-01-23 15:55:03 +0000

    Add files via upload

---
+++ DAL/transactionDAL.cs
@@ -0,0 +1,119 @@
+﻿using AnyStore.BLL;
+using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace AnyStore.DAL
+{
+    class TransactionDAL
+    {
+        private static readonly string ConnectionString = ConfigurationManager.ConnectionStrings["connstrng"].ConnectionString;
+
+        #region Insert Transaction Method
+        public bool InsertTransaction(TransactionBLL t, out int transactionID)
+        {
+            bool isSuccess = false;
+            transactionID = -1;
+
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                try
+                {
+                    string sql = "INSERT INTO tbl_transactions (type, dea_cust_id, grandTotal, transaction_date, tax, discount, added_by) VALUES (@type, @dea_cust_id, @grandTotal, @transaction_date, @tax, @discount, @added_by); SELECT @@IDENTITY;";
+
+                    using (SqlCommand cmd = new SqlCommand(sql, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@type", t.type);
+                        cmd.Parameters.AddWithValue("@dea_cust_id", t.dea_cust_id);
+                        cmd.Parameters.AddWithValue("@grandTotal", t.grandTotal);
+                        cmd.Parameters.AddWithValue("@transaction_date", t.transaction_date);
+                        cmd.Parameters.AddWithValue("@tax", t.tax);
+                        cmd.Parameters.AddWithValue("@discount", t.discount);
+                        cmd.Parameters.AddWithValue("@added_by", t.added_by);
+
+                        conn.Open();
+
+                        object o = cmd.ExecuteScalar();
+
+                        if (o != null)
+                        {
+                            transactionID = Convert.ToInt32(o);
+                            isSuccess = true;
+                        }
+                        else
+                        {
+                            isSuccess = false;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+
+            return isSuccess;
+        }
+        #endregion
+
+        #region Method to Display All Transactions
+        public DataTable DisplayAllTransactions()
+        {
+            DataTable dt = new DataTable();
+
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                try
+                {
+                    string sql = "SELECT * FROM tbl_transactions";
+
+                    using (SqlCommand cmd = new SqlCommand(sql, conn))
+                    {
+                        SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                        conn.Open();
+                        adapter.Fill(dt);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+
+            return dt;
+        }
+        #endregion
+
+        #region Method to Display Transactions Based on Transaction Type
+        public DataTable DisplayTransactionByType(string type)
+        {
+            DataTable dt = new DataTable();
+
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                try
+                {
+                    string sql = "SELECT * FROM tbl_transactions WHERE type=@type";
+
+                    using (SqlCommand cmd = new SqlCommand(sql, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@type", type);
+
+                        SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                        conn.Open();
+                        adapter.Fill(dt);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+
+            return dt;
+        }
+        #endregion
+    }
+}
---
+++ DAL/transactionDetailDAL.cs
@@ -0,0 +1,54 @@
+﻿using AnyStore.BLL;
+using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace AnyStore.DAL
+{
+    class TransactionDetailDAL
+    {
+        // Create Connection String
+        private static readonly string ConnectionString = ConfigurationManager.ConnectionStrings["connstrng"].ConnectionString;
+
+        #region Insert Method for Transaction Detail
+        public bool InsertTransactionDetail(TransactionDetailBLL td)
+        {
+            // Create a boolean value and set its default value to false
+            bool isSuccess = false;
+
+            // Create a database connection here
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                try
+                {
+                    // SQL Query to Insert Transaction details
+                    string sql = "INSERT INTO tbl_transaction_detail (product_id, rate, qty, total, dea_cust_id, added_date, added_by) VALUES (@product_id, @rate, @qty, @total, @dea_cust_id, @added_date, @added_by)";
+
+                    // Passing the value to the SQL Query
+                    using (SqlCommand cmd = new SqlCommand(sql, conn))
+                    {
+                        // Passing the values using cmd
+                        cmd.Parameters.AddWithValue("@product_id", td.product_id);
+                        cmd.Parameters.AddWithValue("@rate", td.rate);
+                        cmd.Parameters.AddWithValue("@qty", td.qty);
+                        cmd.Parameters.AddWithValue("@total", td.total);
+                        cmd.Parameters.AddWithValue("@dea_cust_id", td.dea_cust_id);
+                        cmd.Parameters.AddWithValue("@added_date", td.added_date);
+                        cmd.Parameters.AddWithValue("@added_by", td.added_by);
+
+                        // Open Database connection
+                        conn.Open();
+
+                        // Declare the int variable and execute the query
+                        int rows = cmd.ExecuteNonQuery();
+
+                        isSuccess = rows > 0;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+
